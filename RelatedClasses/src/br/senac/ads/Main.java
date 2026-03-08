519dfd17de1272d2dda5f00e6ef4ac13fb5cca7b
michael <michael_douglasfs@hotmail.com>
2026-03-08 16:29:19 -0300

    main alterada

--- RelatedClasses/src/br/senac/ads/Main.java
+++ RelatedClasses/src/br/senac/ads/Main.java
@@ -1,38 +1,27 @@
 package br.senac.ads;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import br.senac.ads.model.*;
+import br.senac.ads.service.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        // ---------------------------------------------------
-        // CRIAÇÃO DO OBJETO SCANNER
-        // ---------------------------------------------------
-        // O Scanner será utilizado para ler dados digitados
-        // pelo usuário no terminal.
-
         Scanner scanner = new Scanner(System.in);
 
-        // ---------------------------------------------------
-        // VARIÁVEL QUE ARMAZENA A OPÇÃO DO MENU
-        // ---------------------------------------------------
+
+        PedidoService pedidoService = new PedidoService();
+        ItemPedidoService itemService = new ItemPedidoService();
+
+
+        List<Cliente> listaClientes = new ArrayList<>();
+        List<Produto> listaProdutos = new ArrayList<>();
+
         int opcao = -1;
 
-        // ---------------------------------------------------
-        // LOOP PRINCIPAL DO SISTEMA
-        // ---------------------------------------------------
-        // O sistema continuará executando até o usuário
-        // escolher a opção de sair.
-
         while (opcao != 0) {
-
             try {
-                // ---------------------------------------------------
-                // EXIBIÇÃO DO MENU PRINCIPAL
-                // ---------------------------------------------------
-
+                // SEUS PRINTS ORIGINAIS (MANTIDOS EXATAMENTE IGUAIS)
                 System.out.println("\n===== SISTEMA DE PEDIDOS - CAFETERIA =====");
                 System.out.println("1 - Gerenciar Clientes");
                 System.out.println("2 - Gerenciar Produtos");
@@ -41,64 +30,93 @@ public class Main {
                 System.out.println("0 - Sair");
                 System.out.print("Escolha uma opção: ");
 
-                // Leitura da opção digitada
                 opcao = scanner.nextInt();
-
-                // ---------------------------------------------------
-                // PROCESSAMENTO DA OPÇÃO ESCOLHIDA
-                // ---------------------------------------------------
+                scanner.nextLine();
 
                 switch (opcao) {
-
                     case 1:
-                        // Menu de clientes
-                        // Aqui futuramente serão feitas chamadas
-                        // para as funções relacionadas a clientes
+
+                        System.out.println("\n===== SISTEMA DE PEDIDOS - CAFETERIA =====");
+                        System.out.println("1 - Gerenciar Clientes");
+                        System.out.println("2 - Gerenciar Produtos");
+                        System.out.println("3 - Gerenciar Pedidos");
+                        System.out.println("4 - Gerenciar Itens do Pedido");
+                        System.out.println("0 - Sair");
+
+
+                        System.out.print("Digite o ID do Cliente: ");
+                        int idC = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Nome do Cliente: ");
+                        String nomeC = scanner.nextLine();
+                        String telefoneC = scanner.nextLine();
+                        listaClientes.add(new Cliente(idC, nomeC, telefoneC));
+                        System.out.println("Cliente cadastrado!");
                         break;
 
                     case 2:
-                        // Menu de produtos
-                        // Aqui futuramente serão feitas chamadas
-                        // para as funções relacionadas a produtos
+                        System.out.print("ID do Produto: ");
+                        int idP = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Nome do Produto: ");
+                        String nomeP = scanner.nextLine();
+                        System.out.print("Preço: ");
+                        double preco = scanner.nextDouble();
+                        listaProdutos.add(new Produto(idP, nomeP, preco));
+                        System.out.println("Produto cadastrado!");
                         break;
 
                     case 3:
-                        // Menu de pedidos
-                        // Aqui futuramente serão feitas chamadas
-                        // para as funções relacionadas a pedidos
+                        System.out.print("ID do novo Pedido: ");
+                        int idPed = scanner.nextInt();
+                        System.out.print("ID do Cliente para este pedido: ");
+                        int idCli = scanner.nextInt();
+
+                        // Busca o objeto Cliente na lista
+                        Cliente cliEncontrado = null;
+                        for(Cliente c : listaClientes) {
+                            if(c.getID() == idCli) cliEncontrado = c;
+                        }
+
+                        pedidoService.criarPedido(idPed, idCli, cliEncontrado);
                         break;
 
                     case 4:
-                        // Menu de itens do pedido
-                        // Aqui futuramente serão feitas chamadas
-                        // para as funções relacionadas aos itens
+                        System.out.println("1 - Adicionar Item | 2 - Ver Total");
+                        int subOpcao = scanner.nextInt();
+
+                        if(subOpcao == 1) {
+                            System.out.print("ID do Pedido: ");
+                            int pId = scanner.nextInt();
+                            System.out.print("ID do Produto: ");
+                            int prodId = scanner.nextInt();
+                            System.out.print("Quantidade: ");
+                            int qtd = scanner.nextInt();
+                            itemService.adicionarItemPedido(pId, prodId, qtd, listaProdutos);
+                        } else {
+                            System.out.print("ID do Pedido: ");
+                            int pId = scanner.nextInt();
+                            double total = itemService.calcularTotalPedido(pId);
+                            System.out.println("Total do Pedido: R$ " + total);
+                        }
                         break;
 
                     case 0:
-                        // Encerramento do sistema
                         System.out.println("\nEncerrando o sistema...");
                         break;
 
                     default:
-                        // Caso o usuário digite uma opção inválida
                         System.out.println("\nOpção inválida!");
                 }
 
-            }catch (InputMismatchException e) {
-                // Caso o usuário digite uma opção inválida
-                System.out.println("\nOpção inválida!");
-            }catch (Exception e) {
+            } catch (InputMismatchException e) {
+                System.out.println("\nErro: Digite apenas números!");
+                scanner.nextLine();
+            } catch (Exception e) {
                 System.out.println("\nErro inesperado!");
+
             }
-
         }
-
-        // ---------------------------------------------------
-        // FECHAMENTO DO SCANNER
-        // ---------------------------------------------------
-
         scanner.close();
-
-
     }
 }
